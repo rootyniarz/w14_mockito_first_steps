dcaa305a84b324273b5cebea923cab74d7df297d
Damian Fatejów <rootyniarz@users.noreply.github.com>
2023-05-21 17:42:43 +0200

    9. Testing sampleMethod. Bez Springa, Mockito, Mockowanie metod statycznych przez Mockito inline

--- src/main/java/pl/zajavka/example/StaticMethodExample.java
+++ src/main/java/pl/zajavka/example/StaticMethodExample.java
@@ -4,12 +4,9 @@ import java.time.LocalTime;
 
 public class StaticMethodExample {
 
-    public int getNano(){
-        LocalTime now = getNow();
+    public int getNano() {
+        LocalTime now = LocalTime.now();
         return now.getNano();
     }
 
-    public LocalTime getNow() {
-        return LocalTime.now();
-    }
 }
--- src/test/java/pl/zajavka/example/StaticMethodExampleTest.java
+++ src/test/java/pl/zajavka/example/StaticMethodExampleTest.java
@@ -3,16 +3,15 @@ package pl.zajavka.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalTime;
 
-@ExtendWith(MockitoExtension.class)
 class StaticMethodExampleTest {
 
-    @Spy
     private StaticMethodExample staticMethodExample = new StaticMethodExample();
 
     @Test
@@ -22,10 +21,14 @@ class StaticMethodExampleTest {
         int nanoNow = now.getNano();
         LocalTime nowEarlier = now;//.minusNanos(100);
         int nanoEarlier = nowEarlier.getNano();
-        Mockito.when(staticMethodExample.getNow()).thenReturn(nowEarlier);
 
-        //when
-        int result = staticMethodExample.getNano();
+
+        int result;
+        try (MockedStatic<LocalTime> timeMock = Mockito.mockStatic(LocalTime.class)){
+            timeMock.when(LocalTime::now).thenReturn(nowEarlier);
+            //when
+            result = staticMethodExample.getNano();
+        }
 
         //then
         Assertions.assertEquals(result, nanoEarlier);
